63c0778c9f275e8901b34d20ee6674e29210199d
Emdi Gemini <yougotjinx@gmail.com>
2025-11-08 21:53:49 +0800

    first commit

---
+++ my-feed/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './Main.scss'
+import "@fontsource/montserrat"
+import { LoadingScreen } from './components/LoadingScreen'
+
+function App() {
+
+  return (
+    <>
+      <LoadingScreen />
+    </>
+  )
+}
+
+export default App
