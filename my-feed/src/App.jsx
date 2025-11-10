4f872b1427701dea5611285de65825dda6f2966e
Emdi Gemini <yougotjinx@gmail.com>
2025-11-10 12:21:11 +0800

    store all post data and render it to my feed page

--- my-feed/src/App.jsx
+++ my-feed/src/App.jsx
@@ -5,15 +5,20 @@ import { Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { Startup } from './components/Startup'
 import { HomePage } from './Pages/Homepage';
+import { MediaProvider } from './components/MediaContext';
+import { PostProvider } from './components/PostContext';
 
 function App() {
-
   return (
     <>
-      <Routes>
-        <Route element={<Startup />} />
-        <Route index element={<HomePage />} />
-      </Routes>
+      <MediaProvider>
+        <PostProvider>
+          <Routes>
+            <Route element={<Startup />} />
+            <Route index element={<HomePage />} />
+          </Routes>
+        </PostProvider>
+      </MediaProvider>
     </>
   )
 }
--- my-feed/src/components/PostCreator.jsx
+++ my-feed/src/components/PostCreator.jsx
@@ -1,8 +1,10 @@
-import { useState , useRef, useEffect } from "react";
+import { useState , useRef, useEffect, useContext } from "react";
+import { PostContext } from "./PostContext";
+import { MediaContext } from "./MediaContext";
 
 export function PostCreator(){
   const [createPost, setCreatePost] = useState(false);
-  const [mediaFiles, setMediaFiles] = useState([]);
+  const {mediaFiles, setMediaFiles} = useContext(MediaContext);
 
   function postEditor(){
     setCreatePost(true);
@@ -15,7 +17,7 @@ export function PostCreator(){
 
   return (
     <>
-      {createPost && <CreatePost mediaFiles={mediaFiles} setMediaFiles={setMediaFiles} postCreator={postCreator} />}
+      {createPost && <CreatePost postCreator={postCreator} setCreatePost={setCreatePost} />}
       <div onClick={postEditor} className="post-creator">
         <div className="app-icon">
           <img src="../../public/icons/my-feed.svg" alt="My Feed" />
@@ -25,48 +27,26 @@ export function PostCreator(){
   );
 }
 
-function CreatePost({mediaFiles, setMediaFiles, postCreator}){
-  const [fontSize, setFontSize] = useState(16);
+function CreatePost({postCreator, setCreatePost}){
+  const {mediaFiles, setMediaFiles} = useContext(MediaContext);
+  const [postText, setPostText] = useState("");
   const inputField = useRef(null);
-  const fontSizeField = useRef(null);
-  const fileInput = useRef(null);
 
-  const addMedia = () => fileInput.current.click();
-
-  const handleFiles = (e) => {
-    const files = Array.from(e.target.files);
-    setMediaFiles(prev => [...prev, ...files]);
-    e.target.value = "";
-  }
+  useEffect(() => {
+    inputField.current.focus();
+  }, []);
 
   const removeFile = (index) => {
     setMediaFiles(prev => prev.filter((_, i) => i !== index));
   }
 
   const writePost = () => {
-    const postText = inputField.current.textContent;
-    if(inputField && postText.length > 0){
+    const postField = inputField.current.textContent;
+    setPostText(postField);
+    if(inputField && postField.length > 0){
       inputField.current.classList.remove('empty');
     } else {
       inputField.current.classList.add('empty');
-    }
-  }
-
-  const adjustFontSize = () => {
-    const size = fontSizeField.current.value;
-    if(size > 9){
-      setFontSize(size);
-      document.documentElement.style.setProperty('--adjust_FontSize', `${size}px`);
-    }
-  }
-
-  const inputSize = (e) => {
-    if(e.target.value > 64) e.target.value = 64;
-  }
-
-  const enterSize = (e) => {
-    if(e.key === 'Enter' && e.target.value !== ''){
-      adjustFontSize();
     }
   }
 
@@ -83,43 +63,113 @@ function CreatePost({mediaFiles, setMediaFiles, postCreator}){
           {mediaFiles.map((file, i) => {
             const url = URL.createObjectURL(file);
             return(
-              <div className="media" key={i}>
-                <i onClick={() => removeFile(i)} key={`${file}_${i}`} className="bi bi-x"></i>
-                <img className="file" key={`${file}-${i}`} src={url} alt={file.name} />
+              <div className="media" key={`${file.name}-${i}`}>
+                <i onClick={() => removeFile(i)} className="bi bi-x"></i>
+                <img className="file" src={url} alt={file.name} />
               </div>
             )
           })}
         </div>
-        <div className="post-actions">
-          <div className="post-extras">
-            <div className="add-media">
-              <i onClick={addMedia} className="bi bi-image"></i>
-              <input
-                ref={fileInput}
-                onChange={handleFiles}
-                type="file"
-                id="fileInput"
-                accept="image"
-                multiple
-              />
-              <div className="media-label">
-                <p>Add a photo <br /> or a video</p>
-              </div>
-            </div>
-            <div className="adjust-font-size">
-              <label htmlFor="adjFontSize">
-                Adjust font size
-              </label>
-              <div className="input-wrapper">
-                <button onClick={adjustFontSize}><i className="bi bi-fonts"></i></button>
-                <input ref={fontSizeField} onInput={inputSize} onKeyDown={enterSize} id="adjFontSize" type="number" placeholder={`${fontSize}px`} />
-              </div>
-            </div>
-          </div>
+        <PostActions postText={postText} setCreatePost={setCreatePost} />
+      </div>
+    </div>
+  )
+}
 
-          <button className="submit-post">POST</button>
+function PostActions({postText, setCreatePost}){
+  const {mediaFiles, setMediaFiles} = useContext(MediaContext);
+  const {postData, setPostData} = useContext(PostContext);
+  const fileInput = useRef(null);
+
+  const [fontSize, setFontSize] = useState(14);
+  const fontSizeField = useRef(null);
+
+  const inputSize = (e) => {
+    if(e.target.value > 64) e.target.value = 64;
+  }
+
+  const enterSize = (e) => {
+    if(e.key === 'Enter' && e.target.value !== ''){
+      adjustFontSize();
+    }
+  }
+
+  const adjustFontSize = () => {
+    const size = fontSizeField.current.value;
+    if(size > 7){
+      setFontSize(size);
+      document.documentElement.style.setProperty('--adjust_FontSize', `${size}px`);
+    }
+  }
+
+  const handleFiles = (e) => {
+    const files = Array.from(e.target.files);
+    setMediaFiles(prev => [...prev, ...files]);
+    e.target.value = "";
+  }
+
+  const upload = () => {
+    if(postText.replace(/\s/g, '').length === 0) return;
+    
+    const timestamp = new Date();
+
+    if(postData.length > 0){
+      const sortedData = [...postData, {
+        postText,
+        fontSize,
+        mediaFiles: [...mediaFiles],
+        timestamp
+      }].sort((a, b) => b.timestamp - a.timestamp);
+      setPostData(sortedData);
+      localStorage.setItem("postData", JSON.stringify(sortedData));
+    } else {
+      console.log('1st data');
+      setPostData(prev => {
+      const newData = [...prev,
+        {
+          postText,
+          fontSize,
+          mediaFiles: [...mediaFiles],
+          timestamp
+      }];
+      localStorage.setItem("postData", JSON.stringify(newData));
+      return newData;
+      });
+    }
+    
+    setFontSize(14);
+    setCreatePost(false);
+  }
+
+  return (
+    <div className="post-actions">
+      <div className="post-extras">
+        <div className="add-media">
+          <i onClick={() => fileInput.current.click()} className="bi bi-image"></i>
+          <input
+            ref={fileInput}
+            onChange={handleFiles}
+            type="file"
+            id="fileInput"
+            accept="image"
+            multiple
+          />
+          <div className="media-label">
+            <p>Add a photo <br /> or a video</p>
+          </div>
+        </div>
+        <div className="adjust-font-size">
+          <label htmlFor="adjFontSize">
+            Adjust font size
+          </label>
+          <div className="input-wrapper">
+            <button onClick={adjustFontSize}><i className="bi bi-fonts"></i></button>
+            <input ref={fontSizeField} onInput={inputSize} onKeyDown={enterSize} id="adjFontSize" type="number" placeholder={`${fontSize}px`} />
+          </div>
         </div>
       </div>
+
+      <button onClick={upload} className="submit-post">POST</button>
     </div>
   )
 }
